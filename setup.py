7820c12004e4a0e001af48a97b3ba9f368cfb698
unknown <edu230991@gmail.com>
2018-12-20 17:53:05 +0100

    small changes

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ import os
 
 from cx_Freeze import setup, Executable
 
-
+dll_path = r'C:\Anaconda\Library\bin'
 files = [file for file in os.listdir() if (('.bat' in file) or ('accdb' in file))]
+dlls = [os.path.join(dll_path, file) for file in os.listdir() if '.dll' in file] + [r'C:\Anaconda\DLLs\sqlite3.dll']
+
+include_files = files + dlls
 
 build_exe_options = {
     "packages": ["os", "pandas", "pulp", "numpy", "pandas_datareader", "openpyxl"],
-    "includes": [],
-    "include_files": files}
+    "include_files": include_files}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
